e0254c48ae8c3a2cc44407c781f34bad2e74971e
Junior Avelino <junior.a98@hotmail.com>
2022-06-03 15:42:51 -0300

    refactoring starts scripts

--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-require('./database/database').then(() => {
-    const Main = require('./bin/Main')
-    Main.start()
-}).catch((err) => {
-    console.log('erro ao conectar ao banco de dados', err)
-});
